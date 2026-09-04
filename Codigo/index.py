2a265f929d380f0e2818df1d1480ca93f3bc1d76
Jessx6 <jessibenitez0601@gmail.com>
2026-09-04 10:26:52 -0500

    Update index.py

---
+++ Codigo/index.py
@@ -0,0 +1,29 @@
+import random as r
+
+persona = {"vida":200,
+           "vida_max":200,
+           "energia":500,
+           "energia_max":500
+}
+
+villano = {"vida":200,
+           "vida_max":200
+}
+
+ataque1 = {"nombre":"corte_fantasma",
+           "daño_min":10,
+           "daño_max":30,
+           "costo_energia":30
+}
+
+ataque2 = {"nombre":"Furia Arcana",
+           "daño_min":40,
+           "daño_max":60,
+           "costo_energia":50
+}
+
+ataque3 = {"nombre":"Cataclismo Oscuro",
+           "daño_min":70,
+           "daño_max":100,
+           "costo_energia":100
+}
